15c07770ccdf04c633939a272069fabcd42a950d
Noa Ferman <noaf7@uw.edu>
2024-02-14 10:19:19 -0800

    add address toString()

--- back-end/src/Address.java
+++ back-end/src/Address.java
@@ -1,11 +1,12 @@
 public class Address {
-    String nameOfLocation;
-    String line1;
-    String line2;
-    String city;
-    String state;
-    int zipcode;
+    private final String nameOfLocation;
+    private final String line1;
+    private final String line2;
+    private final String city;
+    private final String state;
+    private final int zipcode;
 
+    // city, state required
     public Address(String nameOfLocation, String line1, String line2, String city, String state, int zipcode){
         this.nameOfLocation = nameOfLocation;
         this.line1 = line1;
@@ -14,4 +15,29 @@ public class Address {
         this.state = state;
         this.zipcode = zipcode;
     }
+
+    public String toString(){
+        String res = "";
+        if(nameOfLocation != null){
+            res += nameOfLocation + "/n";
+        }
+
+        if(line1 != null){
+            res += line1 + "/n";
+        }
+
+        if(line2 != null){
+            res += line2 + "/n";
+        }
+
+        res += city + ", " + state; 
+
+        if(zipcode != null){
+            res += " " + zipcode;
+        }
+
+        return res;
+    }
+
+
 }
