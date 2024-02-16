1c9133b2afeb3066ece5750e9b8795c5610c94fd
Noa Ferman <noaferman@github.com>
2024-02-16 14:10:23 -0800

    Spring framework works! Need to reorganize the backend lol

--- back-end/src/maven/src/main/java/com/example/wander/WanderApplication.java
+++ back-end/src/maven/src/main/java/com/example/wander/WanderApplication.java
@@ -1,7 +1,4 @@
-package com.example.wander;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class WanderApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WanderApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
+    public static void main(String[] args) {
+      SpringApplication.run(WanderApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
