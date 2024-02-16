dced159ba5c4625561924793cf35232d4f5de95f
Noa Ferman <noaferman@github.com>
2024-02-16 14:05:58 -0800

    testing spring framework

---
+++ back-end/src/maven/src/main/java/com/example/wander/WanderApplication.java
@@ -0,0 +1,24 @@
+package com.example.wander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
