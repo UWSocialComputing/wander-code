f7c946b521bccf4a236ea7c20b99816fd40de7df
Jaela Field <jaelaf@uw.edu>
2024-02-26 10:10:30 -0800

    cors fix

--- back-end/src/main/java/com/example/wander/WanderApplication.java
+++ back-end/src/main/java/com/example/wander/WanderApplication.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.google.gson.Gson;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @SpringBootApplication
 @RestController
 public class WanderApplication {
